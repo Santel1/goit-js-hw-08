bf4cfed12a9bb9bd5427cd55ce25de2e6b0d7f9a
Santel1 <126976920+Santel1@users.noreply.github.com>
2023-07-28 15:29:47 +0200

    hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const player = new Player('vimeo-player', {
+  id: 19231868,
+  width: 640,
+});
+
+player.on('timeupdate', throttle(playerTime, 1000));
+
+function playerTime(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem(LOCALSTORAGE_KEY, currentTime);
+}
+
+const time = localStorage.getItem(LOCALSTORAGE_KEY);
+
+player
+  .setCurrentTime(time)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const throttle = require('lodash.throttle');
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(textContent, 500));
+
+function textContent(evt) {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+window.addEventListener('load', () => {
+  const formData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (formData) {
+    emailInput.value = formData.email || '';
+    messageInput.value = formData.message || '';
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(formData);
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  form.reset();
+});
